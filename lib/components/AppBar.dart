0e66c892a3f33d8325a72c35d7fe2d914a1e496f
Varunan_V <47948397+varunan-vara@users.noreply.github.com>
2020-07-13 13:39:00 -0400

    Added Navigation Bar and App Bar

---
+++ lib/components/AppBar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+AppBar myAppBar(){
+  return AppBar(
+    elevation: 0,
+    leading: IconButton(
+      icon: Icon(Icons.menu),
+      onPressed: () {},
+    ),
+  );
+}
---
+++ lib/pages/myapp.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import './../components/AppBar.dart';
+
+//imports for Navbar
+import './Sleep/sleep.dart';
+import './Settings/settings.dart';
+import './Feed/feed.dart';
+import './Diary/diary.dart';
+
+//ThemeData
+
+const kPrimaryColor = Color.fromRGBO(40, 83, 107, 1);
+const kTextColor = Color.fromRGBO(126, 168, 190, 1);
+const kBackgroundColor = Color.fromRGBO(246, 240, 237, 1);
+const double kDefaultPadding = 20.0;
+
+class MyApp extends StatelessWidget {
+  //This is the main app Navigation Widget
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Cranberry",
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Mainpage(),
+    );
+  }
+}
+
+
+class Mainpage extends StatefulWidget {
+  MainState createState() => MainState();
+}
+
+
+class MainState extends State<Mainpage> {
+
+  //Navigation for bottomNavigationBar
+  int _currentIndex = 0;
+  String currentPage = "Home";
+
+  //homes for navigation bar
+  final List<Widget> _children = [
+    MainFeed(),
+    SleepFeed(),
+    DiaryFeed(),
+    Settings()
+  ];
+
+  final List<String> _pageNames = [
+    "Home",
+    "Sleep",
+    "Diary",
+    "Settings"
+  ];
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: myAppBar(),
+      body: MainBody(_children[_currentIndex], _pageNames[_currentIndex]),
+      bottomNavigationBar: Container(
+        height: 60,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0,-10),
+              blurRadius: 35,
+              color: kPrimaryColor.withOpacity(0.38),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home),
+              color: kPrimaryColor,
+              onPressed: () {onTabTapped(0);},
+            ),
+            IconButton(
+              icon: Icon(Icons.timer),
+              color: kPrimaryColor,
+              onPressed: () {onTabTapped(1);},
+            ),
+            Container(
+              width: 30,
+            ),
+            IconButton(
+              icon: Icon(Icons.book),
+                color: kPrimaryColor,
+              onPressed: () {onTabTapped(2);},
+            ),
+            IconButton(
+              icon: Icon(Icons.settings),
+              color: kPrimaryColor,
+              onPressed: () {onTabTapped(3);},
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
+
+
+
+class MainBody extends StatelessWidget {
+
+  Widget wydget;
+  String page;
+
+  MainBody(this.wydget, this.page);
+@override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: <Widget>[
+        Container(
+          height: size.height *.1,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(left: kDefaultPadding),
+                height: size.height * .15 - 20,
+                color: kPrimaryColor,
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Text(page, style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                      ),),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(child: wydget),
+      ],
+    );
+  }
+}
