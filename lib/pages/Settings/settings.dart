70a3bf730e1c5c069be74e5038fb37a3db8eb726
Varunan_V <47948397+varunan-vara@users.noreply.github.com>
2020-07-13 18:07:10 -0400

    Finished Settings Page

--- lib/pages/Settings/settings.dart
+++ lib/pages/Settings/settings.dart
@@ -2,6 +2,7 @@ import 'package:cranberry/godfile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:app_settings/app_settings.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 class Settings extends StatelessWidget {
 
@@ -52,7 +53,10 @@ class Settings extends StatelessWidget {
               title: Text("Leave a Rating"),
               subtitle: Text("A Rating on the App Store would be very helpful!"),
               onTap: (){
-
+                showDialog(context: context,
+                builder: (BuildContext context) {
+                  return Rating();
+                });
               },
             ),
           ],
@@ -157,18 +161,147 @@ class IssuePage extends StatefulWidget{
 }
 
 class Issue extends State<IssuePage>{
+  final _formKey = GlobalKey<FormState>();
+
+  var body;
+  var subject;
+
+  Future<void> sendEmail() async {
+    final Email email = Email(
+      body: "An Issue has been reported for CranBerry! \n Issue: " + body,
+      subject: 'Cranberry - Issue - ' + subject,
+      recipients: ['varunanvara@gmail.com'],
+      isHTML: false,
+    );
+
+    await FlutterEmailSender.send(email);
+    print("Successfully Sent Email");
+  }
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         elevation: 0,
         title: Text("Report an Issue"),
       ),
-      body: Column(
-        children: <Widget>[
-
-        ],
+      body: Form(
+          key: _formKey,
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+                children: <Widget>[
+                  TextFormField(
+                      decoration: InputDecoration(
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(25.0),
+                          borderSide: new BorderSide(
+                          ),),
+                        fillColor: Color.fromRGBO(126, 168, 190, 0.5),
+                        hoverColor: Color.fromRGBO(126, 168, 190, 1),
+                        focusColor: Color.fromRGBO(40, 83, 107, 1),
+                        hintText: "Brief Description of Issue",
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        } else {
+                          setState(() {
+                            subject = value;
+                          });
+                        }
+                        return null;
+                      }
+                  ),
+                  Text(""),
+                  TextFormField(
+                      decoration: InputDecoration(
+                      border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(25.0),
+                          borderSide: new BorderSide(
+                          ),),
+                        fillColor: Color.fromRGBO(126, 168, 190, 0.5),
+                        hoverColor: Color.fromRGBO(126, 168, 190, 1),
+                        focusColor: Color.fromRGBO(40, 83, 107, 1),
+                        hintText: "Name (Optional)"
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      }
+                  ),
+                  Text(""),
+                  TextFormField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 70, horizontal: 10),
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(25.0),
+                          borderSide: new BorderSide(
+                          ),),
+                        fillColor: Color.fromRGBO(126, 168, 190, 0.5),
+                        hoverColor: Color.fromRGBO(126, 168, 190, 1),
+                        focusColor: Color.fromRGBO(40, 83, 107, 1),
+                        hintText: "Details Regarding the Issue",
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        } else {
+                          setState(() {
+                            body = value;
+                          });
+                        }
+                        return null;
+                      }
+                  ),
+                  FlatButton(
+                    child: Text("Done"),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text("Thank you for reporting the issue!"),
+                            content: Text("We will do what we can to address the problem"),
+                            actions: <Widget>[
+                              FlatButton(
+                                onPressed: () {
+                                  Navigator.pop(context, true);
+                                  Navigator.pop(context);
+                                },
+                                child: Text("Done"),
+                              ),
+                            ],
+                          );
+                        }
+                      );
+                      sendEmail();
+                    },
+                  ),
+                ]
+            ),
+          ),
       ),
+    );
+  }
+}
+
+class Rating extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("This App is not on the Play Store!!"),
+      content: Text("Try again later"),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            Navigator.pop(context, true);
+          },
+          child: Text("Done"),
+        ),
+      ],
     );
   }
 }
