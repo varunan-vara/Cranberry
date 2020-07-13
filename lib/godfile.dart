585b3218e135a7f27cf6a0f462a753c14d515bce
Varunan_V <47948397+varunan-vara@users.noreply.github.com>
2020-07-13 16:34:04 -0400

    Modified Settings Pane, Added 'GodFile'

---
+++ lib/godfile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+var showSleep = true;
+var showDiary = true;
+
+void changeSleep() {
+  showSleep = !showSleep;
+}
+
+void changeDiary() {
+  showDiary = !showDiary;
+}
--- lib/pages/Settings/settings.dart
+++ lib/pages/Settings/settings.dart
@@ -1,3 +1,4 @@
+import 'package:cranberry/components/AppBar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -18,12 +19,40 @@ class Settings extends StatelessWidget {
             ListTile(
               title: Text("Edit Home Feed"),
               subtitle: Text("Choose what you want to see on the Home Page"),
-              onTap: (){},
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => EditHomeFeed()),
+                );
+              },
             ),
             ListTile(
               title: Text("Notification Settings"),
               subtitle: Text("Opens Settings App"),
-              onTap: (){},
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => EditNotif()),
+                );
+              },
+            ),
+            Divider(),
+            ListTile(
+              title: Text("Report Issue"),
+              subtitle: Text("If you spot an issue, please let us know here!"),
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => IssuePage()),
+                );
+              },
+            ),
+            ListTile(
+              title: Text("Leave a Rating"),
+              subtitle: Text("A Rating on the App Store would be very helpful!"),
+              onTap: (){
+
+              },
             ),
           ],
         ),
@@ -32,7 +61,16 @@ class Settings extends StatelessWidget {
   }
 }
 
-
+class Divider extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromRGBO(126, 168, 190, 1),
+      height: .9,
+      width: 500,
+    );
+  }
+}
 class EditHomeFeed extends StatefulWidget{
   EditHome createState() => EditHome();
 }
@@ -42,7 +80,57 @@ class EditHome extends State<EditHomeFeed> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        elevation: 0,
+        title: Text("Edit Home Screen"),
+      ),
+    );
+  }
+}
+
+class EditNotif extends StatefulWidget{
+  Notif createState() => Notif();
+}
+
+class Notif extends State<EditNotif> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text("Notification Settings"),
+      ),
+      body: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text("Edit Notification Settings"),
+            subtitle: Text("This will redirect you to the settings page"),
+            onTap: (){},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class IssuePage extends StatefulWidget{
+  Issue createState() => Issue();
+}
+
+class Issue extends State<IssuePage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text("Report an Issue"),
+      ),
+      body: Column(
+        children: <Widget>[
+
+        ],
+      ),
     );
   }
 }
